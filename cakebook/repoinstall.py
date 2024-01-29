97c05e8ab0aa1c1f1cf1bb2d0ddd5b03ee5b0add
VIKASH SAINI <143334443+vikash-saini-maruti@users.noreply.github.com>
2024-01-29 14:04:55 +0530

    Project Updated

---
+++ cakebook/repoinstall.py
@@ -0,0 +1,33 @@
+
+'''
+To install dependencies automatically, run the Repoinstall.py program. 
+Before doing so, it is advised to activate the virtual environment command with:
+envdigital\Scripts\activate
+
+'''
+
+
+import subprocess
+
+def install_requirements(file_path):
+    failed_packages = []
+
+    with open(file_path, 'r') as file:
+        for line in file:
+            package = line.strip()
+            try:
+                subprocess.run(['pip', 'install', package], check=True)
+            except subprocess.CalledProcessError as e:
+                print(f"Error installing {package}: {e}")
+                failed_packages.append(package)
+
+    if failed_packages:
+        print("\nPackages that encountered errors:")
+        for package in failed_packages:
+            print(package)
+    else:
+        print("\nAll packages installed successfully.")
+
+if __name__ == "__main__":
+    requirements_file = "requirements.txt"
+    install_requirements(requirements_file)
